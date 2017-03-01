482742285aa35456bfecfc50792167de2fdb3378
Raghu Betina <raghu@firstdraft.com>
2017-03-01 23:26:02 +0000

    Direct association between properties and bookmarks

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,6 +1,8 @@
 class Bookmark < ApplicationRecord
   # Direct associations
 
+  belongs_to :property
+
   belongs_to :user,
              :class_name => "Investor",
              :foreign_key => "investor_id"
--- app/models/property.rb
+++ app/models/property.rb
@@ -1,6 +1,9 @@
 class Property < ApplicationRecord
   # Direct associations
 
+  has_many   :bookmarks,
+             :dependent => :destroy
+
   # Indirect associations
 
   # Validations
