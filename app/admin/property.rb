dc9aaf0e4089a10c130fa52af834349805687c93
Raghu Betina <raghu@firstdraft.com>
2017-03-01 23:26:27 +0000

    Whitelist columns in Property for ActiveAdmin

--- app/admin/property.rb
+++ app/admin/property.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Property do
 
+ permit_params :airbnb_price, :listing_price, :sublease_price, :region_id, :pathway_id
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
