d47ad41c2eaa3f2a1ffb3ff27cb9cf4efd7e1f49
Raghu Betina <raghu@firstdraft.com>
2017-03-01 23:25:53 +0000

    Direct association between investors and bookmarks

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,6 +1,10 @@
 class Bookmark < ApplicationRecord
   # Direct associations
 
+  belongs_to :user,
+             :class_name => "Investor",
+             :foreign_key => "investor_id"
+
   # Indirect associations
 
   # Validations
--- app/models/investor.rb
+++ app/models/investor.rb
@@ -1,6 +1,9 @@
 class Investor < ApplicationRecord
   # Direct associations
 
+  has_many   :bookmarks,
+             :dependent => :destroy
+
   # Indirect associations
 
   # Validations
