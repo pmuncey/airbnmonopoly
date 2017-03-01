fac21aee5dc663b4ee4746cedcb48a386271530f
Raghu Betina <raghu@firstdraft.com>
2017-03-01 23:26:03 +0000

    Direct association between pathways and properties

--- app/models/pathway.rb
+++ app/models/pathway.rb
@@ -1,6 +1,9 @@
 class Pathway < ApplicationRecord
   # Direct associations
 
+  has_many   :properties,
+             :dependent => :destroy
+
   has_many   :bookmarks,
              :dependent => :destroy
 
